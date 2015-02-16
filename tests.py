64bf5dd99064014e599971deba94ca0fc5b093e3
kengreg <ken-angel@hotmail.com>
2015-02-16 01:33:16 -0500

    test modificado de acuerdo a pagina 29

--- tests.py
+++ tests.py
@@ -12,8 +12,7 @@ from librokyudo.views import home_page
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve ('/')
-        self.assertEqual(found.func, home_page)
-    
+        self.assertEqual(found.func, home_page)   
     def test_home_page_returns_correct_html(self):
         request = HttpRequest() 
         response = home_page(request)
